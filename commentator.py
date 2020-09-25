153f09218709c9246d74e55c36526fbd120eff75
Thierry Seegers <surfintheusa@yahoo.com>
2020-09-25 17:09:44 +0200

    Implement left-justify program argument

--- commentator.py
+++ commentator.py
@@ -12,9 +12,11 @@ import threading
 ARGPARSER = argparse.ArgumentParser()
 ARGPARSER.add_argument('paths', nargs='+', type=str, help='Path(s) of file(s), folder(s) or glob expression.')
 ARGPARSER.add_argument('-c', '--compress', dest='compress', action='store_true', default=False,
-                       help='Leaves white space but no newlines between comments.')
+                       help='Leaves no newlines between comments preserving their indentation.')
 ARGPARSER.add_argument('-d', '--dry-run', dest='dry_run', action='store_true', default=False,
                        help='Doesn\'t write \"comments\" files. (Useful for --stats')
+ARGPARSER.add_argument('-l', '--left-justify', dest='left_justify', action='store_true', default=False,
+                       help='Justifies all coments to the left.')
 ARGPARSER.add_argument('-p', '--parallel', dest='parallel', action='store_true', default=False,
                        help='Analyzes multiple files in parallel.')
 ARGPARSER.add_argument('-r', '--recursive', dest='recursive', action='store_true', default=False,
@@ -49,29 +51,35 @@ def comment(path):
             # In order to perfectly take pathological cases in to account, we compute the number of newlines to prefix 
             # this comment with and, if any, the text preceding it based on the placement of the previous comment token 
             # (none, earlier line or same line). Existing whitespace is kept as is, all other text is converted to space.
-            # If ARGS.compress is True, we minimize the ammount newlines and comments are left justified.
+            # If ARGS.compress is True, we minimize the ammount newlines.
+            # If ARGS.left_justify is True, comments are left justified.
             # We also keep tabs on the comment's number of lines for statistics purposes.
-            newlines = 0
-            preceding_text = str()
+            newlines = 1
+            preceding_text = ''
             if previous_comment_token is None:
                 if ARGS.compress:
                     newlines = 0
                 else:
-                    newlines = token.extent.start.line - 1
+                     newlines = token.extent.start.line - 1
+                if not ARGS.left_justify:
                     preceding_text = source[token.extent.start.line - 1][:token.extent.start.column - 1]
                 comment_lines += token.extent.end.line - token.extent.start.line + 1
             elif previous_comment_token.extent.end.line != token.extent.start.line:
-                if ARGS.compress:
-                    newlines = 1
-                else:
+                if not ARGS.compress:
                     newlines = token.extent.start.line - previous_comment_token.extent.end.line
+                if not ARGS.left_justify:
                     preceding_text = source[token.extent.start.line - 1][:token.extent.start.column - 1]
                 comment_lines += token.extent.end.line - token.extent.start.line + 1
             else:
+                newlines = 0
                 preceding_text = source[token.extent.start.line - 1][previous_comment_token.extent.end.column - 1:token.extent.start.column - 1]
                 comment_lines += token.extent.end.line - token.extent.start.line
 
-            comments += '\n' * newlines + re.sub(r'\S', ' ', preceding_text) + (re.sub(r'\n\s+', '\n', spelling) if ARGS.compress else spelling)
+            # Best effort left-justification for multiline comments. Imperfect when tabs are used.
+            if ARGS.left_justify:
+                spelling = re.sub(r'\n\s{' + str(token.extent.start.column - 1) + '}', '\n', spelling)
+
+            comments += '\n' * newlines + re.sub(r'\S', ' ', preceding_text) + spelling
             previous_comment_token = token
 
     # Actually produce a file if required.
@@ -79,6 +87,7 @@ def comment(path):
     if not ARGS.dry_run:
         destination = open(path.parent.joinpath(path.stem + '.comments' + path.suffix), 'w')
         destination.write(comments)
+        destination.close()
 
     # Return this data for stats collection.
     return len(source), comment_lines
